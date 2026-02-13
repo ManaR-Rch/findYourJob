9e43e19ac2343869b2833dac13f16e1e47add2e2
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-13 15:56:14 +0100

    create job card component with favorite and apply buttons

---
+++ src/app/components/job-card/job-card.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Job } from '../../models/job.model';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-job-card',
+  standalone: true,
+  imports: [CommonModule, DatePipe],
+  templateUrl: './job-card.component.html'
+})
+export class JobCardComponent {
+  @Input() job!: Job;
+  @Input() isLoggedIn = false;
+  @Input() isFavorite = false;
+
+  @Output() addFavorite = new EventEmitter<Job>();
+  @Output() removeFavorite = new EventEmitter<Job>();
+  @Output() addApplication = new EventEmitter<Job>();
+
+  onToggleFavorite() {
+    if (this.isFavorite) {
+      this.removeFavorite.emit(this.job);
+    } else {
+      this.addFavorite.emit(this.job);
+    }
+  }
+
+  onAddApplication() {
+    this.addApplication.emit(this.job);
+  }
+}
