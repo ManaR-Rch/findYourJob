1e2119983881340ab720e088be199ba1eaac1e5c
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-13 15:57:26 +0100

    create pagination component

---
+++ src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html'
+})
+export class PaginationComponent {
+  @Input() currentPage = 1;
+  @Input() totalPages = 1;
+  @Output() pageChange = new EventEmitter<number>();
+
+  get pages(): number[] {
+    const pages: number[] = [];
+    const start = Math.max(1, this.currentPage - 2);
+    const end = Math.min(this.totalPages, this.currentPage + 2);
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChange.emit(page);
+    }
+  }
+}
