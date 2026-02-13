c69ed0112f6b8ad8124998378bb4b69472adf075
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-13 15:29:16 +0100

    create loading spinner component

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  template: `
+    <div class="flex justify-center items-center py-8">
+      <div class="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+    </div>
+  `
+})
+export class LoadingComponent {}
