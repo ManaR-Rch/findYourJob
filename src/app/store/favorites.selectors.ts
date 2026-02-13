e68f71e615fb284589ff2305a0c3ac07ce2049ac
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-13 16:11:22 +0100

    create ngrx favorites selectors

---
+++ src/app/store/favorites.selectors.ts
@@ -0,0 +1,19 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { FavoritesState } from './favorites.reducer';
+
+export const selectFavoritesState = createFeatureSelector<FavoritesState>('favorites');
+
+export const selectAllFavorites = createSelector(
+  selectFavoritesState,
+  (state) => state.favorites
+);
+
+export const selectFavoritesLoading = createSelector(
+  selectFavoritesState,
+  (state) => state.loading
+);
+
+export const selectFavoritesError = createSelector(
+  selectFavoritesState,
+  (state) => state.error
+);
