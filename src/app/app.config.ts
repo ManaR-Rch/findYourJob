0e22fd9ca1b92e3933b62a9004b5227f81376a6a
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-15 16:22:23 +0000

    register ngrx store and effects in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { favoritesReducer } from './store/favorites.reducer';
+import { FavoritesEffects } from './store/favorites.effects';
 
 import { routes } from './app.routes';
 
@@ -8,6 +13,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideStore({ favorites: favoritesReducer }),
+    provideEffects([FavoritesEffects]),
+    provideStoreDevtools({ maxAge: 25 })
   ]
 };
