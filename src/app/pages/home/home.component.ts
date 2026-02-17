e20f80b0e45ef191525a0339583eb5baf47b1b8d
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-17 13:50:15 +0000

    use home features child component in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HomeFeaturesComponent } from './home-features/home-features.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HomeFeaturesComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
