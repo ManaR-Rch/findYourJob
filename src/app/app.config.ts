8669de8f73e68025a8f8e04808edbe92bc42ec3a
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-17 13:44:02 +0000

    register error interceptor in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { favoritesReducer } from './store/favorites.reducer';
 import { FavoritesEffects } from './store/favorites.effects';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 import { routes } from './app.routes';
 
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     provideStore({ favorites: favoritesReducer }),
     provideEffects([FavoritesEffects]),
     provideStoreDevtools({ maxAge: 25 })
