83fc02c8cd1921c0f1ed5af4cb6177f577e3d5d3
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-19 14:10:33 +0000

    ajout scroll to top dans le composant principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ToastComponent } from './components/toast/toast.component';
+import { ScrollTopComponent } from './components/scroll-top/scroll-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, ToastComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ToastComponent, ScrollTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
