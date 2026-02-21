8146764f86de595bbbb7609fa593845f208a8c38
ManaR-Rch <manarmarchou6@gmail.com>
2026-02-21 15:30:02 +0000

    ajout texte de chargement sous le spinner

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-loading',
   standalone: true,
   template: `
-    <div class="flex justify-center items-center py-8">
+    <div class="flex flex-col justify-center items-center py-8">
       <div class="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+      <p class="text-gray-400 text-sm mt-3">Chargement...</p>
     </div>
   `
 })
